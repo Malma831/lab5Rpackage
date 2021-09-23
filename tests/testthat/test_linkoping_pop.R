da8096589d3d480424df02dda2ed045e56a404cb
Malma831 <89968327+Malma831@users.noreply.github.com>
2021-09-23 11:35:37 +0200

    tests

---
+++ tests/testthat/test_linkoping_pop.R
@@ -0,0 +1,34 @@
+context("linkoping_pop")
+
+test_that("Variables names are correct", {
+  data<-linkoping_pop()
+
+  expect_equal(colnames(data), c("Year","gender","value"))
+  expect_equal(length(data),3)
+
+})
+
+test_that("Class is correct", {
+  data<- linkoping_pop()
+
+  expect_s3_class(data, "data.frame")
+})
+
+test_that("Wrong input throws an error.", {
+  expect_error(linkoping_pop("100"))
+  expect_error(linkoping_pop(100))
+  expect_error(linkoping_pop(TRUE))
+})
+
+test_that("Variables types are correct", {
+  data<- linkoping_pop()
+
+  bool_list <- lapply(data, FUN = function(x){return(is.numeric(x) | is.factor(x))})
+
+  expect_equal(do.call("all", bool_list), TRUE)
+})
+
+
+
+
+
